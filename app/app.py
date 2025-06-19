5dade76fbf9713113ec0ba5172081beee9ca7614
Rosmeri Ccanto Flores <rosmeri.ccanto@unmsm.edu.pe>
2025-06-18 23:11:16 -0500

    Monitoreo SRE con Flask + Prometheus + Grafana y Docker

--- app/app.py
+++ app/app.py
@@ -36,3 +36,6 @@ def error():
 @app.route("/metrics")
 def metrics():
     return generate_latest(), 200, {'Content-Type': 'text/plain; version=0.0.4'}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
